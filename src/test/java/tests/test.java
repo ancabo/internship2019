861293733ef73a4363609c59ed318d9e532edab2
Andrei Petrusel <andrei.petrusel.ext@siemens.com>
2019-07-17 10:08:03 +0300

    added reports and logging to test

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import commons.Helpers;
+import commons.LogType;
 import commons.TestBase;
 import pages.Header;
 import pages.Home;
@@ -27,10 +28,10 @@ public class test extends TestBase {
 		helper = new Helpers();
 		footer = new Footer(driver);
 	}
-	
 
 	@Test
 	public void bookOverOneYear() throws InterruptedException {
+		logReport(LogType.INFO, "Booking test started started.");
 		SoftAssert noOverbooking = new SoftAssert();
 		home.setFrameAndCheckin();
 		home.clickInDay(helper.azi());
@@ -40,15 +41,18 @@ public class test extends TestBase {
 		home.clickOutDay(helper.azi());
 		home.setFrameAndClickSearchBtn();
 		noOverbooking.assertEquals(room.isOverBook(), true);
+		logReport(LogType.INFO, "Booking test ended.");
 		noOverbooking.assertAll();
 
 	}
 
 	@Test
 	public void checkChatButton() throws InterruptedException {
+		logReport(LogType.INFO, "Chat button test started.");
 		SoftAssert chatWorks = new SoftAssert();
 		footer.setFrameAndClickChat();
 		chatWorks.assertEquals(footer.isChatOpen(), true);
+		logReport(LogType.INFO, "Chat button functionality verrified.");
 		chatWorks.assertAll();
 
 	}
@@ -56,6 +60,7 @@ public class test extends TestBase {
 	@Test
 	public void bookingRoomVerify() throws InterruptedException {
 		SoftAssert softAssert = new SoftAssert();
+		logReport(LogType.INFO, "Room booking test started");
 		// SoftAssert outDateGreyed = new SoftAssert();
 		// SoftAssert correctTimeFrame = new SoftAssert();
 		Thread.sleep(4000);
@@ -73,9 +78,11 @@ public class test extends TestBase {
 		//// We verify if the date 24 august is clickable
 		Thread.sleep(1000);
 		softAssert.assertEquals(room.isOutDateClickable("24"), false);
+		logReport(LogType.INFO, "Verification that the button is unclickable complete.");
 
 		// verify color
 		softAssert.assertEquals(room.isOutDateGreyed("24"), true);
+		logReport(LogType.INFO, "Verification that the button greyed out is complete.");
 
 		///// set check out to 31 aug
 		room.clickOutDay("31");
@@ -86,17 +93,17 @@ public class test extends TestBase {
 
 		// we verify the timeframe is correct
 		softAssert.assertEquals(room.isCorrectTimeFrame(), true);
+		logReport(LogType.INFO, "Verification of the timeframe is complete.");
 
 		// click highest rate room
 		room.roomHighestRate();
 		room.roomClickCheckOut();
 		softAssert.assertEquals(room.isOutDateClickable("27"), false);
+		logReport(LogType.INFO, "Verification that the outdate is unclickableon the desired room page is complete.");
 		softAssert.assertEquals(room.isPriceRight(), true);
+		logReport(LogType.INFO, "Verification of the total price is complete.");
 		softAssert.assertAll();
 
 	}
-	
-	
-	
 
 }
