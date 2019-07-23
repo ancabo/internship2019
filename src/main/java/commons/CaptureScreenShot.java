0411d5d1b58f5bd5058b31991c33879b7757e979
Stefana Ros <stefana.ros.ext@siemens.com>
2019-07-23 12:51:53 +0300

    CaptureScreen modified

--- src/main/java/commons/CaptureScreenShot.java
+++ src/main/java/commons/CaptureScreenShot.java
@@ -48,6 +48,9 @@ public class CaptureScreenShot {
 		 File source = ts.getScreenshotAs(OutputType.FILE);
 		                //after execution, you could see a folder "FailedTestsScreenshots" under src folder
 		 String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/"+screenshotName+dateName+".png";
+		 System.out.println("Destination1: " + destination);
+		 String destination2 = "./FailedTestsScreenshots/"+screenshotName+dateName+".png";
+		 System.out.println("Destination2:" + destination2);
 		 File finalDestination = new File(destination);
 		 FileUtils.copyFile(source, finalDestination);
 		 return destination;
