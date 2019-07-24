0a6fe72a63fc80f8ecc742be7c3410c3490986c5
Andrei Petrusel <andrei.petrusel.ext@siemens.com>
2019-07-24 13:11:05 +0300

    added data provider class

---
+++ src/main/java/commons/DataProviderBase.java
@@ -0,0 +1,24 @@
+package commons;
+
+import java.lang.reflect.Method;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderBase {
+	@DataProvider(name = "DateProvider")
+	public static Object[][] getDatesProvider(Method met) {
+		if(met.getName().equalsIgnoreCase("bookTestA")) {
+		return new Object[][] { 
+			{ "25", "31" }, 
+			{ "12", "23" }, 
+			{ "15", "26" } 
+		};
+	}
+		else return Object[][] {
+			{},
+			{},
+			{}
+		};
+	}
+
+}
