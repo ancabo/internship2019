1a62cfa917889443ff12cec5adf0eca5a9bd3fd8
Andrei Petrusel <andrei.petrusel.ext@siemens.com>
2019-07-24 14:32:18 +0300

    morewait changes

--- src/main/java/commons/DriverHelpers.java
+++ src/main/java/commons/DriverHelpers.java
@@ -89,7 +89,8 @@ public class DriverHelpers extends TestBase {
 	public void waitForElementVisibility(By by) throws InterruptedException {
 //		for (int i = 0; i <= 5; i++) {
 //			try {
-				wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+				//wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+				wait.until(ExpectedConditions.elementToBeClickable(by));
 //				break;
 //			} catch (Exception e) {
 //				Thread.sleep(2000);
