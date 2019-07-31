7333f6662825402f92993bb21040e6ab466c80c6
Stefana Ros <stefana.ros.ext@siemens.com>
2019-07-31 10:53:31 +0300

    Testbase changed

--- src/main/java/commons/DataProviderBase.java
+++ src/main/java/commons/DataProviderBase.java
@@ -1,7 +1,15 @@
 package commons;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.testng.annotations.DataProvider;
 
 public class DataProviderBase {
@@ -14,4 +22,27 @@ public class DataProviderBase {
 	public static Object[][] getDatesProvider() {
 		return new Object[][] { { "25", "24", "31", "27" }, { "14", "13", "23", "16" } };
 	}
-}
+	
+	@DataProvider(name = "dangerousData")
+	public static Object[][] getDates() throws IOException {
+		File file = new File("C:\\Users\\z0043dba\\eclipse-workspace\\intership\\internship2019\\Data_PMI.xls");
+		FileInputStream inputStream = new FileInputStream(file);
+		Workbook bookerbook = null;
+		bookerbook = new HSSFWorkbook(inputStream);
+		Sheet testSheet = bookerbook.getSheet("Sheet1");
+		int lastRowNum = testSheet.getLastRowNum();
+		int lastCellNum = testSheet.getRow(0).getLastCellNum();
+		Object[][] obj = new Object[lastRowNum][1];
+
+		for (int i = 0; i < lastRowNum; i++) {
+			Map<Object, Object> datamap = new HashMap<>();
+			for (int j = 0; j < lastCellNum; j++) {
+				datamap.put(testSheet.getRow(0).getCell(j).toString(), testSheet.getRow(i + 1).getCell(j).toString());
+			}
+			obj[i][0] = datamap;
+
+		}
+		return obj;
+	}
+}  
+
